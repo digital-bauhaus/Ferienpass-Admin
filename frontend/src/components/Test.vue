8f1d0b82009f2783c8391afa76f35bf4baa067a4
Fabi3nn3 <fabienne.hubricht@uni-weimar.de>
2017-12-05 12:26:00 +0100

    small changes

--- frontend/src/components/Test.vue
+++ frontend/src/components/Test.vue
@@ -1,34 +1,15 @@
 <template>
-	<div>
-		<h2>{{title}}</h2>
-		Erstellen von Veranstaltung <br/>
-		Übersicht über Veranstaltung <br/>
-		Übersicht über Kinder in der jeweiligen Veranstaltung <br/>
-		Editing <br/>
-		<br/>
-
-		<br/>
-		<b>Event bearbeiten:</b><br/>
-		<b>Verwaltung</b>:
-		Ich als Admin möchte die Anmeldung zu allen Veranstaltungen einsehen und verwalten können. <br/>
-		-> eventuell Button, drauf klicken und dann weiterleitung an datenbank bzw erstmal dummy daten <br/>
-		<br/>
-		<b>Reservierung:</b>
-		Ich als Admin möchte eine gewisse Anzahl von Veranstaltungsplätzen reservieren können und wieder freigeben können. <br/>
-		-> Reservierung über eine Nummer die dann gesendet wird, platz für veranstaltung eingeben <br/>
-		<br/>
-		<b>Reporting </b>:
-		Ich als Admin möchte die Anmeldung zu einer Veranstaltung exportieren können. Vielleicht als veranstaltungsname.pdf <br/>
-		-> veranstaltung einsehen, über datenbank und dann exportieren <br/>
-		<br/>
-		<b>Stornierung</b>:
-		Ich als Admin möchte bestimmte Anmeldungen stornieren können. <br/>
-		-> eintrag aus datenbank entfernen <br/>
-		<br/>
-		<h1>Hello App!</h1>
-		<p>
-			<a href="/#/Veranstaltung/" >Veranstaltung erstellen</a>
-		</p>
+  <div>
+    <h2>{{title}}</h2>
+    Erstellen von Veranstaltung <br/>
+    Übersicht über Veranstaltung <br/>
+    Übersicht über Kinder in der jeweiligen Veranstaltung <br/>
+    Editing <br/>
+    <br/>
+    <h1>Hello App!</h1>
+    <p>
+      <a href="/#/Veranstaltung/" >Veranstaltung erstellen</a>
+    </p>
     <p>
       <a href="/#/Verwaltung/" >Veranstaltungen verwalten</a>
     </p>
@@ -38,6 +19,25 @@
     <p>
       <a href="/#/Reservierung/" >Reservierungen</a>
     </p>
+
+    <br/>
+    <b>Event bearbeiten:</b><br/>
+    <b>Verwaltung</b>:
+    Ich als Admin möchte die Anmeldung zu allen Veranstaltungen einsehen und verwalten können. <br/>
+    -> eventuell Button, drauf klicken und dann weiterleitung an datenbank bzw erstmal dummy daten <br/>
+    <br/>
+    <b>Reservierung:</b>
+    Ich als Admin möchte eine gewisse Anzahl von Veranstaltungsplätzen reservieren können und wieder freigeben können. <br/>
+    -> Reservierung über eine Nummer die dann gesendet wird, platz für veranstaltung eingeben <br/>
+    <br/>
+    <b>Reporting </b>:
+    Ich als Admin möchte die Anmeldung zu einer Veranstaltung exportieren können. Vielleicht als veranstaltungsname.pdf <br/>
+    -> veranstaltung einsehen, über datenbank und dann exportieren <br/>
+    <br/>
+    <b>Stornierung</b>:
+    Ich als Admin möchte bestimmte Anmeldungen stornieren können. <br/>
+    -> eintrag aus datenbank entfernen <br/>
+    <br/>
 	</div>
 </template>
 
