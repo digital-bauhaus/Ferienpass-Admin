38bf52b44126fa97dd2928a75411e2ae66ecbb00
Fabi3nn3 <fabienne.hubricht@uni-weimar.de>
2018-01-15 18:31:46 +0100

    Veranstaltung kann nach Name sortiert werden, Datum in progress

--- frontend/src/components/Verwaltung.vue
+++ frontend/src/components/Verwaltung.vue
@@ -6,10 +6,10 @@
     <br/>
     <form v-if="formDataLoaded" class="form">
       <br/>
-        <table>
+        <table id='myTable'>
         <tr>
-          <th>Veranstaltung</th>
-          <th>Datum</th>
+          <th v-on:click="sortTable(0)">Veranstaltung</th>
+          <th v-on:click="sortTable(1)">Datum</th>
           <th>Veranstalter</th>
           <th> belegt / gesamt Plätze</th>
           <th>Bearbeiten</th>
@@ -27,6 +27,102 @@
             </td>
           </tr>
       </table>
+      <!--
+      <table id='myTable'>
+        <tr>
+          <th v-on:click="sortTable(0)">Name</th>
+          <th v-on:click="sortTable(1)">Country</th>
+        </tr>
+        <tr>
+          <td>Berglunds snabbkop</td>
+          <td>Schweden</td>
+        </tr>
+        <tr>
+          <td>North/South</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Koniglich Essen</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari</td>
+          <td>Italy</td>
+        </tr>
+      </table>
+      <table class="tbl-general tbl-orders">
+        <thead>
+          <tr>
+            <th class="c1 first-col activesort" onclick="sortByDate()">
+              <a href="#">Date</a>
+            </th>
+            <th class="c2">
+              <a href="#">Order Number</a>
+            </th>
+            <th class="c3">
+              <a href="#">Type</a>
+            </th>
+            <th class="c4 isPriceCell">
+              <a href="#">Order Total</a>
+            </th>
+            <th class="c5">
+              <a href="#">Status</a>
+            </th>
+            <th class="c6">
+              <a href="#">Reorder</a>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="date_elements_class">07-12-2014</td>
+            <td>
+              <a title="view order" class="actLnk" href=""/pcam/account/orders/5475003998">5475003998</a>
+            </td>
+            <td>ONLINE2</td>
+            <td class="isPriceCell">
+              <span$50.75</span>
+            </td>
+            <td>Waiting for Credit Approval</td>
+            <td class="isButtonCell">
+              <a class="button single" href="ezroh/5475003998">Reorder Items</a>
+            </td>
+          </tr>
+          <tr>
+            <td class="date_elements_class">06-12-2014</td>
+            <td>
+              <a title="view order" class="actLnk" href="/pcam/account/orders/5475003998">5475003998</a>
+            </td>
+            <td>ONLINE </td>
+            <td class="isPriceCell">
+              <span>$50.75</span>
+            </td>
+            <td>Waiting for Credit Approval </td>
+            <td class="isButtonCell">
+              <a class="button single" href="ezroh/5475003998">Reorder Items</a>
+            </td>
+          </tr>
+          <tr>
+            <td class="date_elements_class">08-12-2014</td>
+            <td>
+              <a title="view order" class="actLnk" href="/pcam/account/orders/5475003998">5475003998</a>
+            </td>
+            <td>ONLINE1 </td>
+            <td class="isPriceCell">
+              <span>$50.75</span>
+            </td>
+            <td>Waiting for Credit Approval </td>
+            <td class="isButtonCell">
+              <a class="button single" href="ezroh/5475003998">Reorder Items</a>
+            </td>
+          </tr>
+        </tbody>
+
+      </table>-->
     </form>
 
     <footer>
@@ -36,6 +132,7 @@
 	</div>
 </template>
 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script>
 export default {
   name: 'Verwaltung',
@@ -67,6 +164,76 @@ export default {
           this.dummyLoaded = true;
         });
     },
+    sortTable (n) {
+      var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount;
+      switchcount = 0;
+      table = document.getElementById('myTable');
+      switching = true;
+      dir = 'asc';
+      while (switching) {
+        switching = false;
+        rows = table.getElementsByTagName('TR');
+
+        for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName('TD')[n];
+          y = rows[i + 1].getElementsByTagName('TD')[n];
+
+          if (dir === 'asc') {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              shouldSwitch = true;
+              break;
+            }
+          } else if (dir === 'desc') {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+              shouldSwitch = true;
+              break;
+            }
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          switchcount++;
+        } else {
+          if (switchcount === 0 && dir === 'asc') {
+            dir = 'desc';
+            switching = true;
+          }
+        }
+      }
+    },
+    /* sortByDate () {
+      var sortOrder = 'desc';
+      var dateArray = [];
+      var dateMap = {};
+      var dateElements = $jQuery('.date_elements_class');
+
+      $jQuery(dateElements).each(function() {
+        dateMap[$(this).text().replace(/^\s+|\s+$/g, '')] = $(this).parent();
+        dateArray.push($(this).text().replace(/^\s+|$/g, ''));
+      });
+
+      dateArray.sort();
+      dateArray.sort(function(a, b) {
+        a = new Date(a);
+        b = new Date(b);
+        return a>b;
+      });
+
+      if(sortOrder === 'desc') {
+        sortOrder = 'asc';
+      } else {
+        dateArray.reverse();
+        sortOrder = 'desc';
+      }
+
+      jQuery(jQuery("table.tbl-general").find("tbody")[0]).html("");
+
+      for (var i = 0; i < dateArray.length; i++) {
+        jQuery(jQuery("table.tbl-general").find("tbody")[0]).append(dateMap[dateArray[i]]);
+      }
+    }, */
     kill (event) {
       event.target.parentElement.parentElement.parentElement.remove();
     },
