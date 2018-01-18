911d20196f10e4c6796b73341f8aa4b4d5e28cd0
Fabi3nn3 <fabienne.hubricht@uni-weimar.de>
2018-01-18 11:33:10 +0100

    Teilnehmer nach Bezahlstatus und Name sortieren

--- frontend/src/components/Teilnehmer.vue
+++ frontend/src/components/Teilnehmer.vue
@@ -11,10 +11,10 @@
     <main>
     <h1>Teilnehmerübersicht</h1>
 
-       <table v-if="allusers && allusers.length">
+       <table v-if="allusers && allusers.length" id="myTable">
        <tr>
-         <th>Status</th>
-         <th>Name</th>
+         <th v-on:click="sortTable(0)">Status</th>
+         <th v-on:click="sortTable(1)">Name</th>
          <th>Addresse</th>
          <th>Telefon</th>
          <th>Bearbeiten</th>
@@ -68,6 +68,45 @@ export default {
     })
   },
   methods: {
+    sortTable (n) {
+      var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount;
+      switchcount = 0;
+      table = document.getElementById('myTable');
+      switching = true;
+      dir = 'asc';
+      while (switching) {
+        switching = false;
+        rows = table.getElementsByTagName('TR');
+
+        for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName('TD')[n];
+          y = rows[i + 1].getElementsByTagName('TD')[n];
+
+          if (dir === 'asc') {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              shouldSwitch = true;
+              break;
+            }
+          } else if (dir === 'desc') {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+              shouldSwitch = true;
+              break;
+            }
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          switchcount++;
+        } else {
+          if (switchcount === 0 && dir === 'asc') {
+            dir = 'desc';
+            switching = true;
+          }
+        }
+      }
+    },
     kill (event) {
       var modal = document.getElementById('delete');
       modal.style.display = 'block';
